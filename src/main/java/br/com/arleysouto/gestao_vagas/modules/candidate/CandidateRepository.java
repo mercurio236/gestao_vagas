79f031e6563e0ec9d04a0a021d5f909daa2ee231
Arley Souto <macx15@hotmail.com>
2024-05-10 15:48:34 -0300

    feat: validando usuário já existente

--- src/main/java/br/com/arleysouto/gestao_vagas/modules/candidate/CandidateRepository.java
+++ src/main/java/br/com/arleysouto/gestao_vagas/modules/candidate/CandidateRepository.java
@@ -1,11 +1,12 @@
 package br.com.arleysouto.gestao_vagas.modules.candidate;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID>{
+public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID> {
 
-    
-    
+    Optional<CandidateEntity> findByUsernameOrEmail(String username, String email);
+
 }
--- src/main/java/br/com/arleysouto/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/arleysouto/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.arleysouto.gestao_vagas.exceptions.UserFoundException;
 import br.com.arleysouto.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.arleysouto.gestao_vagas.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
@@ -18,8 +19,13 @@ public class CandidateController {
     private CandidateRepository candidateRepository;
 
     @PostMapping("/")
-    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity){
+    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        this.candidateRepository.findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
+                .ifPresent((user) -> {
+                    throw new UserFoundException();
+                });
+
         return this.candidateRepository.save(candidateEntity);
     }
-    
+
 }
