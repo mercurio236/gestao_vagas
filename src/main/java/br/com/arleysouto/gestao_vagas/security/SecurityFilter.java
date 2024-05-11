9b43e0a8c69a2a60a1c68abcaf38b21bd76d80d8
Arley Souto <macx15@hotmail.com>
2024-05-10 23:37:18 -0300

    feat: criado security filter

---
+++ src/main/java/br/com/arleysouto/gestao_vagas/security/SecurityFilter.java
@@ -0,0 +1,46 @@
+package br.com.arleysouto.gestao_vagas.security;
+
+import java.io.IOException;
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import br.com.arleysouto.gestao_vagas.providers.JWTProvider;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired 
+    private JWTProvider jwtProvider;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String header = request.getHeader("Authorization");
+
+        if(header != null){
+           var subToken =  this.jwtProvider.validateToken(header);
+           if(subToken.isEmpty()){
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+           }
+
+           request.setAttribute("company_id", subToken);
+           UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(subToken, null, Collections.emptyList());
+           SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+}
