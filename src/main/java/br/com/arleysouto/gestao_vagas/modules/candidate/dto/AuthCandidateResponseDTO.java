c361c57b3d5b815c14f29ed77a53181a894fca67
Arley Souto <macx15@hotmail.com>
2024-05-11 00:43:34 -0300

    feat: criando token de candidato

---
+++ src/main/java/br/com/arleysouto/gestao_vagas/modules/candidate/dto/AuthCandidateResponseDTO.java
@@ -0,0 +1,14 @@
+package br.com.arleysouto.gestao_vagas.modules.candidate.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor //construtos vazio
+@AllArgsConstructor //construtos com parametros
+public class AuthCandidateResponseDTO {
+    private String access_token;
+}
