18e4784a466fc6e2d81ef734f8887740a381dbb8
Arley Souto <macx15@hotmail.com>
2024-05-12 00:50:26 -0300

    feat: criado documentação da aplicação

--- src/main/java/br/com/arleysouto/gestao_vagas/GestaoVagasApplication.java
+++ src/main/java/br/com/arleysouto/gestao_vagas/GestaoVagasApplication.java
@@ -3,18 +3,7 @@ package br.com.arleysouto.gestao_vagas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-
-
 @SpringBootApplication
-@OpenAPIDefinition(
-	info = @Info(
-		title = "Gestão de vagas",
-		description = "API responsável pela gestão de vagas",
-		version = "1"
-	)
-)
 public class GestaoVagasApplication {
 
 	public static void main(String[] args) {
